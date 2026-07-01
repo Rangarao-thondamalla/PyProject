b93fe8c6edf9f21b804db6c021c21fd598cd76a5
Rangarao Thondamalla <t.rangarao007@gmail.com>
2026-07-01 22:27:50 +0530

    Day 2 topics - explanation of data types

---
+++ day2.py
@@ -0,0 +1,30 @@
+# create the string method examples
+
+x ="Hello, World!"
+
+print(x.upper()) # convert to uppercase
+print(x.lower()) # convert to lowercase
+print(x.strip()) # remove whitespace from the beginning and end
+print(x.replace("H", "J")) # replace a character with another character
+print(x.split(",")) # split the string into a list
+print(x.find("World")) # find the index of a substring
+
+#program1
+x = "10"
+z = " "
+y = "Hello, World!"
+print(x + z + y) # print multiple variables
+
+a = 10
+b = 20
+print(a + b) # add two numbers
+
+names = ["Alice", "Bob", "Charlie"]     
+
+e,r,t = names
+
+print(e) # print the first name
+print(r) # print the second name        
+
+age='25'
+print(int(age)+5)
